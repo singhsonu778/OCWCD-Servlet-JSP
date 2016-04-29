faecf99b92194054fb43f4e864c3dd8253c49d74
Sonu Kumar Singh <singhsonu778@gmail.com>
2016-04-29 20:21:34 +0530

    Add HelloServlet file

---
+++ WEB-INF/src/HelloServlet.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<html><head><title>HelloServlet</title>");
+        out.println("<link href=\"favicon.ico\" rel=\"icon\" type=\"image/x-icon\"/></head>");
+        out.println("<body><p>HelloServlet response!!!</p>");
+        out.println("</body></html>");
+	}
+}
