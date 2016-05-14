d78fdcbfc39c09736cfded19a7bcf18e2b5d6fd6
Sonu Kumar Singh <singhsonu778@gmail.com>
2016-05-14 12:09:08 +0530

    Use doEndTag() method for Classic Tag

--- WEB-INF/src/ClassicTagTest.java
+++ WEB-INF/src/ClassicTagTest.java
@@ -1,19 +1,29 @@
 package com.sonu.domain;
 
 import java.io.IOException;
-
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.TagSupport;
 
 public class ClassicTagTest extends TagSupport {
+	private JspWriter out;
+
 	public int doStartTag() throws JspException {
-		JspWriter out = pageContext.getOut();
+		out = pageContext.getOut();
 		try {
-			out.println("Classic tag output");
+			out.println("in doStartTag()");
 		} catch (IOException ex) {
 			throw new JspException("IOException : " + ex.toString());
 		}
 		return SKIP_BODY;
+	}
+
+	public int doEndTag() throws JspException {
+		try {
+			out.println("<br/>in doEndTag()");
+		} catch (IOException ex) {
+			throw new JspException("IOException : " + ex.toString());
+		}
+		return EVAL_PAGE;
 	}
 }
